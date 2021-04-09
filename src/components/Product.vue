927a6a0d3913e654aa965f79c7a6339318a37c8d
GeartHub <geart.otten@gmail.com>
2021-04-10 00:41:39 +0200

    Added product component

---
+++ src/components/Product.vue
@@ -0,0 +1,12 @@
+<template>
+<v-col cols="12" sm="4" lg="2">
+  <v-row align="center">
+    
+      <v-card>
+        <v-card-title>Pizza pepperoni </v-card-title>
+        Price: €8,00
+      </v-card>
+      </v-row>
+    </v-col>
+  
+</template>
